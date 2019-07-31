0aadce092392636fdb9bf155296639929385be46
Developer EPROC <developer@ubuntudev01.supreme-energy.com>
2019-07-31 19:23:03 +0700

    amendment acceptance fixing

--- app12/views/procurement/V_amendment_acceptance.php
+++ app12/views/procurement/V_amendment_acceptance.php
@@ -2,7 +2,7 @@
     <div class="content-wrapper">
         <div class="content-header row">
             <div class="content-header-left col-md-6 col-12 mb-1">
-                <h3 class="content-header-title"><?= lang("Amendment Acceptante & Supplier Document Submission", "Amendment Acceptante & Supplier Document Submission") ?></h3>
+                <h3 class="content-header-title"><?= lang("Amendment Acceptance & Supplier Document Submission", "Amendment Acceptance & Supplier Document Submission") ?></h3>
             </div>
             <div class="content-header-right breadcrumbs-right breadcrumbs-top col-md-6 col-12">
                 <div class="breadcrumb-wrapper col-12">

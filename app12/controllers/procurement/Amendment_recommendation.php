309b5a631c05b0dd7cf9255b7ee56b263f797db2
Developer EPROC <developer@ubuntudev01.supreme-energy.com>
2019-08-27 16:55:44 +0700

    amd create, approval, view arf revision time ambil dari notification dan agreement periode new dan lates

--- app12/controllers/procurement/Amendment_recommendation.php
+++ app12/controllers/procurement/Amendment_recommendation.php
@@ -93,8 +93,12 @@ class Amendment_recommendation extends CI_Controller {
             }
         }
 
-        foreach ($this->m_arf_detail_revision->where('doc_id', $arf->doc_id)->get() as $revision) {
+        /*foreach ($this->m_arf_detail_revision->where('doc_id', $arf->doc_id)->get() as $revision) {
             $arf->revision[$revision->type] = $revision;
+        }*/
+        $markingType = [1=>'value', 2=>'time', 3=>'scope', 4=>'other'];
+        foreach ($this->db->where('doc_id', $t_arf_notification->id)->get('t_arf_notification_detail_revision')->result() as $revision) {
+            $arf->revision[$markingType[$revision->type]] = $revision;
         }
         $arf->response_attachment = $this->m_arf_response_attachment->where('t_arf_response_attachment.doc_id', $arf->doc_id)
         ->get();
@@ -410,8 +414,12 @@ class Amendment_recommendation extends CI_Controller {
             }
         }
         
-        foreach ($this->m_arf_detail_revision->where('doc_id', $arf->doc_id)->get() as $revision) {
+        /*foreach ($this->m_arf_detail_revision->where('doc_id', $arf->doc_id)->get() as $revision) {
             $arf->revision[$revision->type] = $revision;
+        }*/
+        $markingType = [1=>'value', 2=>'time', 3=>'scope', 4=>'other'];
+        foreach ($this->db->where('doc_id', $t_arf_notification->id)->get('t_arf_notification_detail_revision')->result() as $revision) {
+            $arf->revision[$markingType[$revision->type]] = $revision;
         }
         $arf->response_attachment = $this->m_arf_response_attachment->where('t_arf_response_attachment.doc_id', $arf->doc_id)
         ->get();
@@ -542,7 +550,7 @@ class Amendment_recommendation extends CI_Controller {
     }
     public function newAgreementPeriodTo($arf='')
     {
-        $doc_date = $this->db->where(['po_no'=>$arf->po_no])->order_by('doc_date','desc')->get('t_arf')->row();
+        /*$doc_date = $this->db->where(['po_no'=>$arf->po_no])->order_by('doc_date','desc')->get('t_arf')->row();
         $doc_date = $arf->amended_date;
 
         $detail_revision = $this->m_arf_detail_revision->detail_revision($arf);
@@ -550,15 +558,16 @@ class Amendment_recommendation extends CI_Controller {
         {
             echo $this->db->last_query();
         }
-        $myDate = $doc_date;
+        $myDate = $doc_date;*/
 
-        if($detail_revision->num_rows() > 0)
+        /*if($detail_revision->num_rows() > 0)
         {
           $detail_revision = $detail_revision->row();
           $dariArf = date('Y-m-d', strtotime($doc_date));
           $dariDetail = date('Y-m-d', strtotime($detail_revision->value));
           $myDate = $dariArf > $dariDetail ? $doc_date :$detail_revision->value;
-        }
+        }*/
+        $myDate = getLastTimeAmd($arf->doc_no, $arf->amended_date,"<=");
         return $myDate;
     }
     public function addTime($arf='',$po='')
@@ -721,8 +730,12 @@ class Amendment_recommendation extends CI_Controller {
             }
         }
 
-        foreach ($this->m_arf_detail_revision->where('doc_id', $arf->doc_id)->get() as $revision) {
+        /*foreach ($this->m_arf_detail_revision->where('doc_id', $arf->doc_id)->get() as $revision) {
             $arf->revision[$revision->type] = $revision;
+        }*/
+        $markingType = [1=>'value', 2=>'time', 3=>'scope', 4=>'other'];
+        foreach ($this->db->where('doc_id', $t_arf_notification->id)->get('t_arf_notification_detail_revision')->result() as $revision) {
+            $arf->revision[$markingType[$revision->type]] = $revision;
         }
         $arf->response_attachment = $this->m_arf_response_attachment->where('t_arf_response_attachment.doc_id', $arf->doc_id)->get();
         $arf->performance_bond = $this->m_arf_acceptance_document->view('document')->scope('performance_bond')->where('doc_no', $arf->doc_no)->get();
@@ -795,8 +808,12 @@ class Amendment_recommendation extends CI_Controller {
             }
         }
 
-        foreach ($this->m_arf_detail_revision->where('doc_id', $arf->doc_id)->get() as $revision) {
+        /*foreach ($this->m_arf_detail_revision->where('doc_id', $arf->doc_id)->get() as $revision) {
             $arf->revision[$revision->type] = $revision;
+        }*/
+        $markingType = [1=>'value', 2=>'time', 3=>'scope', 4=>'other'];
+        foreach ($this->db->where('doc_id', $t_arf_notification->id)->get('t_arf_notification_detail_revision')->result() as $revision) {
+            $arf->revision[$markingType[$revision->type]] = $revision;
         }
         $arf->response_attachment = $this->m_arf_response_attachment->where('t_arf_response_attachment.doc_id', $arf->doc_id)
         ->get();

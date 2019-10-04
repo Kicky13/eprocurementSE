cac56ecbb575f396557ec4bc6e0f44dd0d85e75e
alfhan <alfhanz@gmail.com>
2019-10-04 21:20:22 +0700

    partlist #2

--- app12/views/cmms/equipment/part-list.php
+++ app12/views/cmms/equipment/part-list.php
@@ -9,7 +9,7 @@
 	</thead>
 	<tbody>
 		<?php 
-			foreach ($part_list as $r)
+			foreach ($part_list->result() as $r)
 			{
 				echo "<tr><td>$r->ITEM_NUMBER</td><td>$r->DESCRIPTION</td><td>$r->REQUEST</td><td>$r->ACTUAL</td></tr>";
 			}

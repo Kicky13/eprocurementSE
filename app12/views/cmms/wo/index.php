5ea348fb093fd4371c654750faeb44600830dd53
alfhan <alfhanz@gmail.com>
2019-11-19 11:09:20 +0700

    order date & req finish date

--- app12/views/cmms/wo/index.php
+++ app12/views/cmms/wo/index.php
@@ -1,4 +1,7 @@
+<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
 <link rel="stylesheet" type="text/css" href="<?= base_url() ?>ast11/css/custom/custom.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.full.js"></script>
+
 <div class="app-content content">
   <div class="content-wrapper">
     <div class="content-header row">
